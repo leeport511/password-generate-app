9c7e509c38f582e0608a775c1bf38d64aa817262
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2025-02-10 18:47:10 -0300

    se creo archivo indes.js y se esta trabajando en el control del input

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,18 @@
 
 // form
 
+//todo: dar color a la barra cuando cambio el rango si decrece o crece y cambiar ese numero en el documento.
+
+const rangeInput = document.getElementById('characters-range');
+const rangeNumber = document.getElementById('range-number');
+
+rangeInput.addEventListener('input', (e) => {
+
+      let min = Number(e.target.min);
+      let max = Number(e.target.max);
+      let value = Number(e.target.value);
+      
+      
+      rangeNumber.textContent = `${value}`
+
+})
