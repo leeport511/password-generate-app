22af009996e0f38fc6c14db39352a5193ff9ee5c
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2025-02-17 20:16:20 -0300

    terminado logica de password generator

--- scripts/index.js
+++ scripts/index.js
@@ -1,94 +1,145 @@
-const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-const Numbers = "1234567890";
-const Symbols = "!@#$%^&*?";
+const copiedText = document.querySelector('.copied-txt');
+
+const rangeInput = document.getElementById('characters-range');
+const rangeNumber = document.getElementById('range-number');
+let rangeCurrentNumber = Number(rangeInput.value);
+
+const checkboxInputs = document.querySelectorAll('[type=checkbox]');
+const selectedCheckboxes = [];
+
+const typeStrengthTitle = document.getElementById('strength-title');
+const boxes = document.querySelectorAll('.strength-boxes > div');
+
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const Numbers = '1234567890';
+const Symbols = '!@#$%^&*?';
 
 // form
 
 //password-length
 
-const rangeInput = document.getElementById("characters-range");
-const rangeNumber = document.getElementById("range-number");
-let rangeCurrentNumber = Number(rangeInput.value);
-rangeNumber.textContent = "8";
+rangeNumber.textContent = '8';
 
-rangeInput.addEventListener("input", (e) => {
-  let min = Number(e.target.min);
-  let max = Number(e.target.max);
-  let value = Number(e.target.value);
-  rangeCurrentNumber = value;
+rangeInput.addEventListener('input', (e) => {
+	let min = Number(e.target.min);
+	let max = Number(e.target.max);
+	let value = Number(e.target.value);
+	rangeCurrentNumber = value;
 
-  let percentage = ((value - min) / (max - min)) * 100;
+	let percentage = ((value - min) / (max - min)) * 100;
 
-  rangeNumber.textContent = `${value}`;
+	rangeNumber.textContent = `${value}`;
 
-  rangeInput.style.background = `linear-gradient(to right, #A4FFAF 0%, #A4FFAF ${percentage}%, #18171F ${percentage}%, #18171F 100%)`;
+	rangeInput.style.background = `linear-gradient(to right, #A4FFAF 0%, #A4FFAF ${percentage}%, #18171F ${percentage}%, #18171F 100%)`;
 });
 
 //password-characteristics
 
-const checkboxInputs = document.querySelectorAll("[type=checkbox]");
-const selectedCheckboxes = [];
+const selectedCharacterSets = () => {
+	selectedCheckboxes.length = 0;
 
-const addPasswordCharacteristics = () => {
-  selectedCheckboxes.length = 0;
+	if (document.getElementById('uppercase').checked)
+		selectedCheckboxes.push(uppercaseLetters);
+	if (document.getElementById('lowercase').checked)
+		selectedCheckboxes.push(lowercaseLetters);
+	if (document.getElementById('numbers').checked)
+		selectedCheckboxes.push(Numbers);
+	if (document.getElementById('symbols').checked)
+		selectedCheckboxes.push(Symbols);
 
-  checkboxInputs.forEach(({ id, checked }) => {
-    checked && selectedCheckboxes.push(id);
-  });
-
-  console.log(selectedCheckboxes);
-
-  return selectedCheckboxes;
+	return selectedCheckboxes;
 };
 
 // password strength
 
-const typeStrengthTitle = document.getElementById("strength-title");
-const boxes = document.querySelectorAll(".strength-boxes > div");
-const strengthBox1 = document.querySelector(".box-1");
-const strengthBox2 = document.querySelector(".box-2");
-const strengthBox3 = document.querySelector(".box-3");
-const strengthBox4 = document.querySelector(".box-4");
-
 const getPasswordStrength = (e) => {
-  e.preventDefault();
+	typeStrengthTitle.style.display = 'none';
+	selectedCharacterSets();
 
-  typeStrengthTitle.style.display = "none";
-  addPasswordCharacteristics();
+	boxes.forEach((box) => {
+		box.style.backgroundColor = 'inherit';
+		box.style.border = '1px solid #E6E5EA';
+	});
 
-  boxes.forEach((box) => {
-    box.style.backgroundColor = "inherit";
-    box.style.border = "1px solid #E6E5EA";
-  });
+	if (selectedCheckboxes.length === 0) {
+		alert('Please, choose at least one character type');
+		typeStrengthTitle.textContent = '';
 
-  //todo: despues hacer una constante q se el level, actulizar el titulo y hacer un for para aplicar el estilo.
+		return;
+	}
 
-  const strengthStates = [
-    { title: "too weak", color: "#F64A4A", count: 0 },
-    { title: "weak", color: "#FB7C58", count: 1 },
-    { title: "medium", color: "#F8CD65", count: 2 },
-    { title: "medium", color: "#F8CD65", count: 2 },
-    { title: "strong", color: "#A4FFAF", count: 4 },
-  ];
+	const strengthStates = [
+		{ title: 'too weak', color: '#F64A4A', count: 1 },
+		{ title: 'weak', color: '#FB7C58', count: 2 },
+		{ title: 'medium', color: '#F8CD65', count: 3 },
+		{ title: 'strong', color: '#A4FFAF', count: 4 },
+	];
 
-  const level =
-    strengthStates[selectedCheckboxes.length] ||
-    strengthStates[selectedCheckboxes.length - 1];
+	const level = strengthStates[selectedCheckboxes.length - 1];
 
-  typeStrengthTitle.textContent = level.title;
-  typeStrengthTitle.style.display = "flex";
+	typeStrengthTitle.textContent = level.title;
+	typeStrengthTitle.style.display = 'flex';
 
-  for (let i = 0; i <= level.count; i++) {
-    console.log(boxes[i]);
-
-    boxes[i].style.border = "none";
-    boxes[i].style.backgroundColor = level.color;
-  }
+	for (let i = 0; i < selectedCheckboxes.length; i++) {
+		boxes[i].style.border = 'none';
+		boxes[i].style.backgroundColor = level.color;
+	}
 };
 
-// Btn-action ()
+// Generate PassWord
 
-const generateBtn = document.getElementById("generate-btn");
+const generatePassword = (rangeCurrentNumber) => {
+	getPasswordStrength();
+	const selectedCheckboxes = selectedCharacterSets();
 
-generateBtn.addEventListener("click", getPasswordStrength);
+	const allCharacters = selectedCheckboxes.join('');
+	let password = '';
+
+	for (let i = 0; i < rangeCurrentNumber; i++) {
+		const randomIndex = Math.floor(
+			Math.random() * allCharacters.length
+		);
+		password += allCharacters[randomIndex];
+	}
+
+	return password;
+};
+
+let password = '';
+
+const handleGenerateBtnEvent = (e) => {
+	e.preventDefault();
+
+	password = generatePassword(rangeCurrentNumber);
+
+	if (password.includes('undefined')) return;
+
+	document.getElementById('password-gen').value = password;
+};
+
+// Copy to Clipboard
+
+const copyPasswordToClipboard = (password) => {
+	navigator.clipboard
+		.writeText(password)
+		.then(() => {
+			copiedText.style.display = 'block';
+			setTimeout(() => {
+				copiedText.style.display = 'none';
+			}, 1500);
+		})
+		.catch((error) => console.error('Failes to copy', error));
+};
+
+// Btn-action
+
+const generateBtn = document.getElementById('generate-btn');
+
+generateBtn.addEventListener('click', handleGenerateBtnEvent);
+
+const clipboardImage = document.getElementById('clipboard-img');
+
+clipboardImage.addEventListener('click', () =>
+	copyPasswordToClipboard(password)
+);
